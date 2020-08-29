11fa8e072ed15e14209b4b040f56b722e2ad744a
alekh kumar <alekh5007@gmail.com>
2020-08-29 19:36:52 +0530

    Updates

---
+++ precache-manifest.88b88249b86ea1deaf5e94718f8236e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc16e932932be7354e5af6cccddfa399",
+    "url": "/harry_potter/index.html"
+  },
+  {
+    "revision": "44288521bbb56840d48a",
+    "url": "/harry_potter/static/css/main.bcb16289.chunk.css"
+  },
+  {
+    "revision": "f6f0d0559a98cd8c82fc",
+    "url": "/harry_potter/static/js/2.926b4429.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/harry_potter/static/js/2.926b4429.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44288521bbb56840d48a",
+    "url": "/harry_potter/static/js/main.3d818c86.chunk.js"
+  },
+  {
+    "revision": "c621c0d29b68c7075c86",
+    "url": "/harry_potter/static/js/runtime-main.3a61583a.js"
+  },
+  {
+    "revision": "8c1b9daf0d7e26fa03649b4906ec2fc4",
+    "url": "/harry_potter/static/media/bg1.8c1b9daf.png"
+  }
+]);
